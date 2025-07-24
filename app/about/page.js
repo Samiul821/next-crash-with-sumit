692449ba354cb84b8160fc6daced8f80f1be3e1b
Samiul821 <smsamiul821@gmail.com>
2025-07-25 01:03:56 +0600

    Error handling

--- app/about/page.js
+++ app/about/page.js
@@ -1,6 +1,10 @@
 import React from "react";
 
 export default function About() {
+  // throw new Error(
+  //   "There was a huge Error! Please try again later."
+  // );
+
   return (
     <main className="mt-10">
       <div>About Us Page</div>
